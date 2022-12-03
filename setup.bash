f4a6d13fe6b28d38b1571fb26c3bb3a422e1a9ad
Mehti <34041379+mr-mehti@users.noreply.github.com>
2022-12-03 01:31:41 -0700

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,1 +1,4 @@
-
+sudo wget https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh
+sudo chmod +x install.sh
+sudo ./install.sh -n
+wget -N --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && sudo bash bbr.sh -y
